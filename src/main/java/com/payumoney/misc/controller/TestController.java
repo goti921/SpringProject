6332b86078d0be614226b305c6e36d81a06eeb82
Simrandeep Singh <simrandeep.singh@payu.in>
2016-08-29 11:18:25 +0530

    Initial commit Signed-off-by: Simrandeep Singh <simrandeep.singh@payu.in>

---
+++ src/main/java/com/payumoney/misc/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.payumoney.misc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by simrandeep.singh on 8/28/16.
+ */
+@Controller
+public class TestController {
+    @RequestMapping(value = "/miscTest", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody
+    void kafkaTest() {
+        System.out.println("YO YO");
+    }
+}
